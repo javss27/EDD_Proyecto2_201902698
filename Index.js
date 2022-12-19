1839d1f0b883d1fcd26accfe60fda0558d9eba8f
javss27 <92238344+javss27@users.noreply.github.com>
2022-12-19 16:23:49 -0600

    Login

---
+++ Index.js
@@ -0,0 +1,92 @@
+
+// Login
+function login() {  
+    var usuario = document.getElementById("exampleInputEmail1").value;
+    var password = document.getElementById("exampleInputPassword1").value;
+    var checked = document.getElementById("exampleCheck1").checked;
+    if(usuario == "" && password =="" && checked){
+        console.log("Entra admin");
+        document.getElementById("divLogin").style.display = "none";
+        document.getElementById("divAdmin").style.display = "grid";
+    }else{
+        console.log("Busca usuario");
+    }
+}
+
+function cerrarSesion() {
+    document.getElementById("divLogin").style.display = "flex";
+    document.getElementById("divAdmin").style.display = "none";
+}
+// Cargar archivos
+function openFile(event) {
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function() {
+      var text = reader.result;
+      var json = JSON.parse(text);
+      json.forEach(function(cliente) {
+        console.log(cliente);
+    });
+    };
+    reader.readAsText(input.files[0]);
+}
+
+//Lista simple
+class Nodo {
+    constructor(_PersonajeMK) {
+        this.PersonajeMK = _PersonajeMK
+        this.siguiente = null
+    }
+}
+
+class Listasimple{
+    constructor() {
+        this.cabecera = null
+    }
+    agregarPersonajes(_objetoPersonaje) {
+        var tempo = new Nodo(_objetoPersonaje)
+        tempo.siguiente = this.cabecera
+        this.cabecera = tempo
+    }
+
+    graficarlistaPersonajesMarioKart(){
+        var codigodot = "digraph G{\nlabel=\" Mario kart \";\nnode [shape=box];\n graph [rankdir = LR];";
+        var temporal = this.cabecera
+        var conexiones ="";
+        var nodos ="";
+        var numnodo= 0;
+        while (temporal != null) {
+            nodos+=  "N" + numnodo + "[label=\"" + temporal.PersonajeMK.nombre_usuario + "\" ];\n"
+            if(temporal.siguiente != null){
+                var auxnum = numnodo+1
+                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
+            }
+            temporal = temporal.siguiente
+            numnodo++;            
+        }
+        codigodot += "//agregando nodos\n"
+        codigodot += nodos+"\n"
+        codigodot += "//agregando conexiones o flechas\n"
+        codigodot += "{\n"+conexiones+"\n}\n}"
+        //console.log(codigodot)
+        d3.select("#lienzo").graphviz()
+            .width(900)
+            .height(500)
+            .renderDot(codigodot)
+    }
+}
+
+class Cliente{
+    constructor(_dpi,_nombre_completo,_nombre_usuario,_correo,_contrasenia,_telefono){
+        this.dpi = _dpi
+        this.nombre_completo = _nombre_completo
+        this.nombre_usuario = _nombre_usuario
+        this.correo = _correo
+        this.contrasenia = _contrasenia
+        this.telefono = _telefono
+    }
+}
+
+function render() {  
+    listamarioKart.graficarlistaPersonajesMarioKart()    
+}
